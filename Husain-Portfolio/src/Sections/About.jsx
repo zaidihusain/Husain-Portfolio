f5d5535fdce5642b2904befdc90976199c6f8d2e
zaidihusain <zaidihusain611@gmai.com>
2024-10-13 19:30:53 +0530

    Fixed the image

--- Husain-Portfolio/src/Sections/About.jsx
+++ Husain-Portfolio/src/Sections/About.jsx
@@ -33,8 +33,9 @@ const About = () => {
 
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
+            
             <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
-
+          
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
